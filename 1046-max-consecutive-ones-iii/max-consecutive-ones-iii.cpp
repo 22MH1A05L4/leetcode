92c96b69d0ea5f3e6669e5cbc17f749ac761300b
Dhanush <119723718+22MH1A05L4@users.noreply.github.com>
2024-01-29 18:47:25 +0530

    Time: 38 ms (88.80%) | Memory: 57.9 MB (10.18%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0;
+        int j = 0;
+        int countZeros = 0; // Count of zeros in the current window
+        int maxOnes = 0;
+
+        while (j < nums.size()) {
+            if (nums[j] == 0) {
+                countZeros++;
+            }
+
+            // If count of zeros exceeds allowed flips, shrink the window
+            while (countZeros > k) {
+                if (nums[i] == 0) {
+                    countZeros--;
+                }
+                i++;
+            }
+
+            // Update the maximum length
+            maxOnes = max(maxOnes, j - i + 1);
+
+            j++;
+        }
+
+        return maxOnes;
+    }
+};
